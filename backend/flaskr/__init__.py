07dc528a52d3a4b5c5df222aba60a29ae561696e
Destiny <omoruyidestiny30@gmail.com>
2022-09-10 13:49:41 +0100

    fixed endpoint errors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -74,8 +74,11 @@ def create_app(test_config=None):
 
             category_query = Category.query.all()
             
+            
+            categories = {}
             # Looping categories into a dictionary
-            categories = {category.id:category.type for category in category_query}
+            for category in category_query:
+                categories[category.id] = category.type
 
 
             return jsonify({
@@ -83,7 +86,6 @@ def create_app(test_config=None):
                 "questions": current_question,
                 "totalQuestions": len(question_query),
                 "categories": categories,
-                "currentCategory": None
             })
         except:
             abort(404)
@@ -116,10 +118,10 @@ def create_app(test_config=None):
                 if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
                     abort(422)
 
-                new_question = body.get('question', None)
-                new_answer = body.get('answer', None)
-                new_difficulty = body.get('difficulty', None)
-                new_category = body.get('category', None)
+                new_question = body.get('question')
+                new_answer = body.get('answer')
+                new_difficulty = body.get('difficulty')
+                new_category = body.get('category')
 
                 new_question = Question(question=new_question, answer=new_answer, difficulty=new_difficulty, category=new_category)
 
@@ -141,7 +143,6 @@ def create_app(test_config=None):
                     "success":True,
                     "questions": questions,
                     "totalQuestions":len(search_result),
-                    "currentCategory":None,
                 })
             else:
                 abort(422)
@@ -156,8 +157,10 @@ def create_app(test_config=None):
             category = Category.query.filter_by(id=id).first()
 
             question_query = Question.query.filter_by(category=f'{category.id}').all()
-
-            questions = [question.format() for question in question_query]
+            
+            questions = []
+            for question in question_query:
+                questions.append(question.format())
 
             return jsonify({
                 "success": True,
@@ -176,27 +179,28 @@ def create_app(test_config=None):
             body = request.get_json()
 
             if not ('quiz_category' in body and 'previous_questions' in body):
-                abort(422)
+                abort(404)
 
             previous_questions = body.get('previous_questions')
             quiz_category = body.get('quiz_category')
 
-            print(quiz_category['type'])
-
             if quiz_category['type'] == "click":
-                query = Question.query.filter(Question.id.notin_(previous_questions)).all()
+                query = Question.query.all()
             else:
-                query = Question.query.filter_by(category=quiz_category['id']).filter(
-                Question.id.notin_(previous_questions)).all()
+                query = Question.query.filter_by(category=quiz_category['id']).all()
+                
+            question_length = len(query) - 1
+            next_question = query[random.randrange(0, question_length)]
+                
+            if not(next_question.id in previous_questions):
+                return jsonify({
+                    "question": next_question.format(),
+                    "success":True,
+                })
+            else:
+                next_question = query[random.randrange(0, question_length)]
 
 
-            question = query[random.randrange(0, len(query))].format()
-            
-
-            return jsonify({
-                "question": question,
-                "success":True,
-            })
         except:
             abort(422)
 
